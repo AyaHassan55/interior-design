a82312208a351efe5ea39025157ed0ceb668afd0
AyaAly9 <ayaaly929@gmail.com>
2025-08-09 19:31:42 +0300

    responsive

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
 
+    const menuBtn = document.getElementById("menu-btn");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    menuBtn.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+        });
+
+
+// ------------------------------------------------------
+let header = document.querySelector("header");
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+
+
+
+menuIcon.onclick = () => {
+  navbar.classList.toggle("active");
+}
+window.onscroll = () => {
+  navbar.classList.remove("active");
+}
+window.addEventListener('scroll', () => {
+    header.classList.toggle("shadow" , window.scrollY > 0);
+});
+
+
+// ------------------------------------------------------
 const counters = document.querySelectorAll('.counter');
 let started = false;
 
